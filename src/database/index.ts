8e260b3b4679f7f6e4330a66fd6041e5c919c152
Vinicius Hansen D Agostini <94183727+vidagostini1998@users.noreply.github.com>
2023-04-19 11:16:43 -0300

    Initial commit

---
+++ src/database/index.ts
@@ -0,0 +1,40 @@
+import { Sequelize } from "sequelize-typescript";
+import User from "../models/User";
+import Setting from "../models/Setting";
+import Contact from "../models/Contact";
+import Ticket from "../models/Ticket";
+import Whatsapp from "../models/Whatsapp";
+import ContactCustomField from "../models/ContactCustomField";
+import Message from "../models/Message";
+import Queue from "../models/Queue";
+import WhatsappQueue from "../models/WhatsappQueue";
+import UserQueue from "../models/UserQueue";
+import QuickAnswer from "../models/QuickAnswer";
+import Tag from "../models/Tag";
+import ContactTag from "../models/ContactTag";
+
+// eslint-disable-next-line
+const dbConfig = require("../config/database");
+// import dbConfig from "../config/database";
+
+const sequelize = new Sequelize(dbConfig);
+
+const models = [
+  User,
+  Contact,
+  Ticket,
+  Message,
+  Whatsapp,
+  ContactCustomField,
+  Setting,
+  Queue,
+  WhatsappQueue,
+  UserQueue,
+  QuickAnswer,
+  Tag,
+  ContactTag
+];
+
+sequelize.addModels(models);
+
+export default sequelize;
